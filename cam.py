24dae85994f3c834d7e1531c237dbda060f64b5a
unknown <fehimornek@gmail.com>
2023-10-19 18:56:59 +0300

    Turning feed to gray removed, will be added to another function

--- cam.py
+++ cam.py
@@ -1,9 +1,7 @@
-import PIL.Image
-import numpy as np
 import cv2
 
 # video stream taken from the camera live
-def video_stream():
+def camera_stream():
     cv2.namedWindow("preview")
     vc = cv2.VideoCapture(0)
     if vc.isOpened(): # try to get the first frame
@@ -12,18 +10,18 @@ def video_stream():
         rval = False
 
     while rval:
-        # convert from rgb to gray
-        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) 
-
         # live feed as video
         cv2.imshow("preview", frame)
         rval, frame = vc.read()
+        
+        
+        # exit on ESC
         key = cv2.waitKey(20)
-        if key == 27: # exit on ESC
+        if key == 27: 
             break
 
     vc.release()
     cv2.destroyWindow("preview")
 
 
-video_stream()
+camera_stream()
