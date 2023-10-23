4a21c88f77dcbb4534e49038ef7437b86f17a1bf
unknown <fehimornek@gmail.com>
2023-10-23 18:54:05 +0300

    While conversion now each character is spread out the same amount

--- ascii2img.py
+++ ascii2img.py
@@ -11,6 +11,7 @@ def get_text_resolution(ascii_string, font_size = 10):
     return text_width, text_height
 
 def image_creator(ascii_string, ascii_width, ascii_height):
+    ascii_string_mapped = ascii_string.replace('\n','e')
     # create image
     image = Image.new('L', (ascii_width, ascii_height),'white')
     # Create a drawing context
@@ -19,6 +20,27 @@ def image_creator(ascii_string, ascii_width, ascii_height):
     # font is important because cour.tff is a monospaced font. Which means for every character space between is the same.
     font = ImageFont.truetype("cour.ttf", font_size)
     # Draw the text on the image
-    draw.text((0, 0), ascii_string, fill="black", font=font)
-
+    x = 0
+    y = 0
+    for char in ascii_string_mapped:
+        # if \n exists we need to increase y value of the image. To do this we will check \ by doing double \ check
+        if char == 'e':
+            char = ''
+            y += 5
+            x = 0
+        draw.text((x,y), char, fill="black", font=font)
+        x += 5
+        
     image.show()
+
+
+img = Image.open("ang.jpg")
+resized = img_resize(img, new_width=100)
+
+gray = rgb2gray(resized)
+
+ascii_img = pixel2ascii(gray, new_width=100)
+
+ascii_width, ascii_height = get_text_resolution(ascii_img)
+
+image_creator(ascii_img, ascii_width, ascii_height)
