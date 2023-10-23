23d590be85134c642ba8b9c4e26364311bda9278
unknown <fehimornek@gmail.com>
2023-10-23 19:00:14 +0300

    Commepython -u

--- ascii2img.py
+++ ascii2img.py
@@ -11,23 +11,24 @@ def get_text_resolution(ascii_string, font_size = 10):
     return text_width, text_height
 
 def image_creator(ascii_string, ascii_width, ascii_height):
+    # replace values of \n with e because checking for double character is problematic and \ is a pain to check
     ascii_string_mapped = ascii_string.replace('\n','e')
     # create image
     image = Image.new('L', (ascii_width, ascii_height),'white')
     # Create a drawing context
     draw = ImageDraw.Draw(image)
     font_size = 10
-    # font is important because cour.tff is a monospaced font. Which means for every character space between is the same.
     font = ImageFont.truetype("cour.ttf", font_size)
-    # Draw the text on the image
-    x = 0
-    y = 0
+    # start drawing from the origin using x=0 y=0
+    x,y = 0,0
     for char in ascii_string_mapped:
-        # if \n exists we need to increase y value of the image. To do this we will check \ by doing double \ check
+        # if e exists we need to increase y value of the image because it corresponds to \n. Afterwards delete it because 
+        # it served its function as an enter key. Also we need to x = 0 because we want to start drawing from origin again.
         if char == 'e':
             char = ''
             y += 5
             x = 0
+        # Draw the text on the image
         draw.text((x,y), char, fill="black", font=font)
         x += 5
         
